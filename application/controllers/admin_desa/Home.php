abd9f3829e2b098b686df6b24bf2054eab75310b
Rizqy H <rizqy22@gmail.com>
2016-06-08 23:27:51 +0700

    sample for admin template
    selanjutnya bisa dijadikan referensi untuk halaman admin yang lain

---
+++ application/controllers/admin_desa/Home.php
@@ -0,0 +1,32 @@
+<?php
+
+if (!defined('BASEPATH'))
+    exit('No direct script access allowed');
+
+class Home extends CI_Controller
+{
+            
+    function __construct()
+    {
+        parent::__construct();
+        $this->load->library(['ion_auth', 'form_validation']);
+        $this->load->helper('auth_helper');
+
+        $this->load->model('Jurusan_model');
+
+        // sorry, you can not access this page unless you're an admin or admin-desa
+        if (!$this->ion_auth->in_group(['admin', 'admin-desa'])) {
+            redirect(site_url());
+        }
+    }
+
+    public function index()
+    {
+        $data = array(
+            'page_title' => 'Admin Desa',
+        );
+
+        $this->template->load('template', 'admin_desa/home', $data);
+    }
+
+}
---
+++ application/views/admin_desa/home.php
@@ -0,0 +1,17 @@
+
+        <!-- Main content -->
+        <section class='content'>
+          <div class='row'>
+            <div class='col-xs-12'>
+              <div class='box'>
+                <div class='box-header'>
+                  <h3 class='box-title'>JURUSAN LIST</h3>
+                </div><!-- /.box-header -->
+
+                <div class='box-body'>
+                    <p>Selamat datang mas</p>
+                </div><!-- /.box-body -->
+              </div><!-- /.box -->
+            </div><!-- /.col -->
+          </div><!-- /.row -->
+        </section><!-- /.content -->
