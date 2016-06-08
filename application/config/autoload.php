6cb67c03ef65de0fa573061464373057070ca4c9
Rizqy H <rizqy22@gmail.com>
2016-06-09 00:06:10 +0700

    add autoload for helper and set default controller for route

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -83,7 +83,7 @@ $autoload['drivers'] = array();
 |
 |	$autoload['helper'] = array('url', 'file');
 */
-$autoload['helper'] = array('url','form','nuris');
+$autoload['helper'] = array('url','form','nuris', 'auth');
 
 /*
 | -------------------------------------------------------------------
